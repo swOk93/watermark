5df59921b2bad4756582df922abf5679f8ea1981
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-03-24 17:12:34 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -3,60 +3,67 @@ package watermark
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import java.awt.Color
 
 var imageFile = File("")
 var watermarkImageFile = File("")
 
-fun info(name: String) {
-    val imageFile = File(name)
-    if (!imageFile.exists()) {
-        println("The file ${name} doesn't exist.")
-        return
-    }
-    val image: BufferedImage = ImageIO.read(imageFile)
-    println("Image file: ${name}")
-    println("Width: ${image.width}")
-    println("Height: ${image.height}")
-    println("Number of components: ${image.colorModel.numComponents}")
-    println("Number of color components: ${image.colorModel.numColorComponents}")
-    println("Bits per pixel: ${image.colorModel.pixelSize}")
-    when (image.transparency) {
-        1 -> println("Transparency: OPAQUE")
-        2 -> println("Transparency: BITMASK")
-        3 -> println("Transparency: TRANSLUCENT")
-    }
-}
-
-fun checkComponents (name: String, numComponents: Int): Boolean {
-    if (numComponents != 3) {
-        println("The number of \"${name}\" color components isn't 3.")
-        return true
-    } else return false
-}
-
-fun checkBits (name: String, numBits: Int): Boolean {
-    if (numBits != 24 || numBits != 32) {
-        println("The \"${name}\" isn't 24 or 32-bit.")
-        return true
-    } else return false
-}
-
 fun checkAndAssign() {
     println("Input the image filename:")
     imageFile = File(readln())
-    if (!imageFile.exists()) {
-        println("The file ${imageFile.name} doesn't exist.")
-        return
-    }
+    if (!imageFile.exists()) println("The file ${imageFile.path} doesn't exist.").also { return }
     val image: BufferedImage = ImageIO.read(imageFile)
-    if (checkComponents("image", image.colorModel.numColorComponents) || checkBits("image", image.colorModel.pixelSize)) return
+    if (!check("image", image)) return // if false -> return
     println("Input the watermark image filename:")
     watermarkImageFile = File(readln())
-    if (!imageFile.exists()) {
-        println("The file ${watermarkImageFile.name} doesn't exist.")
+    if (!watermarkImageFile.exists()) println("The file ${watermarkImageFile.path} doesn't exist.").also { return }
+    val watermarkImage: BufferedImage = ImageIO.read(watermarkImageFile)
+    if (!check("watermark", watermarkImage)) return
+    if (image.width != watermarkImage.width || image.height != watermarkImage.height) println("The image and watermark dimensions are different.").also { return }
+    blending(image, watermarkImage)
+}
+
+fun blending(image: BufferedImage, watermarkImage: BufferedImage) {
+    println("Input the watermark transparency percentage (Integer 0-100):")
+    var percent = 0
+    try {
+        percent = readln().toInt()
+    } catch (e: NumberFormatException) {
+        println("The transparency percentage isn't an integer number.")
         return
     }
-    if (checkComponents("watermark", image.colorModel.numColorComponents) || checkBits("watermark", image.colorModel.pixelSize)) return
+    if (percent !in 0..100) println("The transparency percentage is out of range.").also { return }
+    println("Input the output image filename (jpg or png extension):")
+    val outputName = readln()
+    if (!(outputName.substring(outputName.length - 4) != ".png").xor(outputName.substring(outputName.length - 4) != ".jpg")) {
+        println("The output file extension isn't \"jpg\" or \"png\".")
+        return
+    }
+    val outputFile = File(outputName)
+    val output: BufferedImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_ARGB)
+    for (x in 0 until image.width) {
+        for (y in 0 until image.height) {
+            val i = Color(image.getRGB(x, y))
+            val w = Color(watermarkImage.getRGB(x, y))
+            val color = Color(
+                (percent * w.red + (100 - percent) * i.red) / 100,
+                (percent * w.green + (100 - percent) * i.green) / 100,
+                (percent * w.blue + (100 - percent) * i.blue) / 100
+            )
+            output.setRGB(x, y, color.rgb)
+        }
+    }
+    ImageIO.write(output, outputName.substring(outputName.length - 3), outputFile)
+    println("The watermarked image ${outputFile.path} has been created.")
+//    println("The watermarked image ${outputFile.name} has been created.")
+}
+
+fun check(name: String, image: BufferedImage): Boolean {
+    val pixelSize = image.colorModel.pixelSize
+    val colorComp = image.colorModel.numColorComponents
+    if (colorComp != 3) println("The number of $name color components isn't 3.").also { return false }
+    if (pixelSize == 24 || pixelSize == 32) return true
+    else println("The $name isn't 24 or 32-bit.").also { return false }
 }
 
 fun main() {
