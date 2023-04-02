f5fa0914b29a074c3393caf2604d0cf4cb8849a4
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-04-02 17:26:50 +0300

    Update main.kt

--- main.kt
+++ main.kt
@@ -24,10 +24,12 @@ fun checkAndAssign() {
 }
 
 fun blending(image: BufferedImage, watermarkImage: BufferedImage) {
-    if (image.transparency == 3) println("Do you want to use the watermark's Alpha channel?")
-    val alpha = if (readln().lowercase() == "yes") true else false
+    val alpha = if (watermarkImage.transparency == 3) {
+        println("Do you want to use the watermark's Alpha channel?")
+        readln().lowercase() == "yes"
+    } else false
     println("Input the watermark transparency percentage (Integer 0-100):")
-    var percent = 0
+    val percent: Int
     try {
         percent = readln().toInt()
     } catch (e: NumberFormatException) {
@@ -42,17 +44,22 @@ fun blending(image: BufferedImage, watermarkImage: BufferedImage) {
         return
     }
     val outputFile = File(outputName)
-    val output: BufferedImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_ARGB)
+    val output = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_ARGB)
     for (x in 0 until image.width) {
         for (y in 0 until image.height) {
-            val i = Color(image.getRGB(x, y), alpha)
-            val w = Color(watermarkImage.getRGB(x, y), alpha)
+            var i = Color(image.getRGB(x, y), alpha)
+            var w = Color(watermarkImage.getRGB(x, y), alpha)
             val color = Color(
                 (percent * w.red + (100 - percent) * i.red) / 100,
                 (percent * w.green + (100 - percent) * i.green) / 100,
                 (percent * w.blue + (100 - percent) * i.blue) / 100
             )
-            output.setRGB(x, y, color.rgb)
+            if (alpha) {
+                if (w.alpha == 0) output.setRGB(x, y, Color(image.getRGB(x, y)).rgb)
+                else output.setRGB(x, y, color.rgb)
+            } else {
+                output.setRGB(x, y, color.rgb)
+            }
         }
     }
     ImageIO.write(output, outputName.substring(outputName.length - 3), outputFile)
@@ -71,4 +78,3 @@ fun check(name: String, image: BufferedImage): Boolean {
 fun main() {
     checkAndAssign()
 //    info(readln())
-}
