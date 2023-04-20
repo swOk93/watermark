cc66b3aec9f23d97352133bd6dffb4d0b6c04e12
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-04-20 21:47:02 +0300

    Restructuring

--- main.kt
+++ main.kt
@@ -6,98 +6,124 @@ import javax.imageio.ImageIO
 import java.awt.Color
 import kotlin.system.exitProcess
 
-var imageFile = File("")
-var watermarkImageFile = File("")
-
-fun checkAndAssign() {
-    println("Input the image filename:")
-    imageFile = File(readln())
-    if (!imageFile.exists()) println("The file ${imageFile.path} doesn't exist.").also { return }
-    val image: BufferedImage = ImageIO.read(imageFile)
-    if (!check("image", image)) return // if false -> return
-    println("Input the watermark image filename:")
-    watermarkImageFile = File(readln())
-    if (!watermarkImageFile.exists()) println("The file ${watermarkImageFile.path} doesn't exist.").also { return }
-    val watermarkImage: BufferedImage = ImageIO.read(watermarkImageFile)
-    if (!check("watermark", watermarkImage)) return
-    if (image.width != watermarkImage.width || image.height != watermarkImage.height) println("The image and watermark dimensions are different.").also { return }
-    blending(image, watermarkImage)
+fun fileCreator(fileName: String): File {
+    val separator = File.separator // get file separator, depends on the system
+    val workingDirectory = System.getProperty ("user.dir") // get working directory
+    val filePath = "${workingDirectory}${separator}" + // form filePath and replace "/" to system separator
+            if ("/" in fileName) fileName.replace("/", separator) else fileName
+    return File(filePath)
 }
 
-fun blending(image: BufferedImage, watermarkImage: BufferedImage) {
-    val alpha = if (watermarkImage.transparency == 3) {
-        println("Do you want to use the watermark's Alpha channel?")
-        readln().lowercase() == "yes"
-    } else false
-    val transpColor = if (watermarkImage.transparency != 3) getTranspColor() else listOf()
-    val percent = getPercent()
-    println("Input the output image filename (jpg or png extension):")
-    val outputName = readln()
-    if (!(outputName.substring(outputName.length - 4) != ".png").xor(outputName.substring(outputName.length - 4) != ".jpg")) {
-        println("The output file extension isn't \"jpg\" or \"png\".")
-        return
-    }
-    val outputFile = File(outputName)
-    val output = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_ARGB)
-    for (x in 0 until image.width) {
-        for (y in 0 until image.height) {
-            val i = Color(image.getRGB(x, y), alpha)
-            val w = Color(watermarkImage.getRGB(x, y), alpha)
-            val color = Color(
-                    (percent * w.red + (100 - percent) * i.red) / 100,
-                    (percent * w.green + (100 - percent) * i.green) / 100,
-                    (percent * w.blue + (100 - percent) * i.blue) / 100
-                )
-                if (alpha) {
-                    if (w.alpha == 0) output.setRGB(x, y, i.rgb)
-                    else output.setRGB(x, y, color.rgb)
-                } else {
-                    output.setRGB(x, y, color.rgb)
-            }
-            if (transpColor.size == 3) {
-                if (w.red == transpColor[0] && w.green == transpColor[1] && w.blue == transpColor[2]) output.setRGB(x, y, i.rgb)
-            }
+class InputImage(type: String) {
+    private val fileName: String
+    private val imageFile: File
+    val image: BufferedImage
+    init {
+        println("Input the ${if (type == "watermark") "watermark image" else type} filename:")
+        fileName = readln()
+        imageFile = fileCreator(fileName) // create image File
+        if (imageFile.exists()) {
+            image = ImageIO.read(imageFile)
+            if (image.colorModel.numColorComponents != 3) println("The number of $type color components isn't 3.").also { exitProcess(0) }
+            if (image.colorModel.pixelSize != 24 && image.colorModel.pixelSize != 32) println("The $type isn't 24 or 32-bit.").also { exitProcess(0) }
+        } else {
+            print("The file $fileName doesn't exist.")
+            exitProcess(0)
         }
     }
-    ImageIO.write(output, outputName.substring(outputName.length - 3), outputFile)
-    println("The watermarked image ${outputFile.path} has been created.")
 }
 
-fun getPercent(): Int {
-    println("Input the watermark transparency percentage (Integer 0-100):")
-    val input = readln()
-    val percent = if (Regex("\\d{1,3}").matches(input)) {
-        input.toInt().also { if (it !in 0..100) {
-            println("The transparency percentage is out of range.").also { exitProcess(-1) }
-        } }
-    } else exitProcess(-1)
-    return percent
-}
-
-fun transpColorInvalid() = println("The transparency color input is invalid.").also { exitProcess(-1) }
-
-fun getTranspColor(): List<Int> {
-    println("Do you want to set a transparency color?")
-    var transpColor = listOf<Int>()
-    if (readln() == "yes") {
-        println("Input a transparency color ([Red] [Green] [Blue]):")
-        transpColor = readln().split(' ').map {
-            if (it.toIntOrNull() != null && it.toIntOrNull() in 0..255) it.toInt()
-            else 0.also { transpColorInvalid() } }
-        if (transpColor.size != 3) transpColorInvalid()
+object OutputImage {
+    private val outputFileName: String
+    private val outputFileExtension: String
+    init {
+        println("Input the output image filename (jpg or png extension):")
+        outputFileName = readln()
+        outputFileExtension = outputFileName.substringAfter(".")
+        if (outputFileExtension != "jpg" && outputFileExtension != "png") {
+            print("The output file extension isn't \"jpg\" or \"png\".").also { exitProcess(0) }
+        }
     }
-    return transpColor
+
+    fun createOutput(image: BufferedImage, watermark: BufferedImage) {
+        val outputImage = Blender.blendImage(image, watermark)
+        val outputFile = fileCreator(outputFileName)
+        ImageIO.write(outputImage, outputFileExtension, outputFile)
+        print("The watermarked image $outputFileName has been created.")
+    }
 }
 
-fun check(name: String, image: BufferedImage): Boolean {
-    val pixelSize = image.colorModel.pixelSize
-    val colorComp = image.colorModel.numColorComponents
-    if (colorComp != 3) println("The number of $name color components isn't 3.").also { return false }
-    if (pixelSize == 24 || pixelSize == 32) return true
-    else println("The $name isn't 24 or 32-bit.").also { return false }
+object Blender {
+    var alpha = false
+    var transparency = false
+    private var transparencyColor = Color(0, 0, 0)
+    private var transparencyPercentage = 0
+    fun setTransparencyColor() {
+        println("Input a transparency color ([Red] [Green] [Blue]):")
+        val input = readln()
+        if (Regex("\\d{1,3} \\d{1,3} \\d{1,3}").matches(input)) {
+            val (red, green, blue) = input.split(" ").map { it.toInt().also {
+                if (it !in 0..255) print("The transparency color input is invalid.").also {exitProcess(0)}
+            } }
+            transparencyColor = Color(red, green, blue)
+        } else print("The transparency color input is invalid.").also { exitProcess(0) }
+    }
+
+    fun setTransparencyPercentage() {
+        println("Input the watermark transparency percentage (Integer 0-100):")
+        val input = readln()
+        if (Regex("\\d{1,3}").matches(input)) {
+            transparencyPercentage = input.toInt().also { if (it !in 0..100) {
+                print("The transparency percentage is out of range.").also { exitProcess(0) }
+            } }
+        } else print("The transparency percentage isn't an integer number.").also { exitProcess(0) }
+    }
+
+    fun blendImage(image: BufferedImage, watermark: BufferedImage): BufferedImage {
+        val outputImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
+
+        for (x in 0 until image.width) {
+            for (y in 0 until image.height) {
+                val i = Color(image.getRGB(x, y))
+                val w = Color(watermark.getRGB(x, y), true)
+                var color = Color(i.red, i.green, i.blue)
+                if (alpha && w.alpha == 0) {
+                    outputImage.setRGB(x, y, color.rgb)
+                } else if (transparency &&
+                    w.red == transparencyColor.red &&
+                    w.green == transparencyColor.green &&
+                    w.blue == transparencyColor.blue) {
+                    outputImage.setRGB(x, y, color.rgb)
+                } else {
+                    color = Color(
+                        (transparencyPercentage * w.red + (100 - transparencyPercentage) * i.red) / 100,
+                        (transparencyPercentage * w.green + (100 - transparencyPercentage) * i.green) / 100,
+                        (transparencyPercentage * w.blue + (100 - transparencyPercentage) * i.blue) / 100)
+                    outputImage.setRGB(x, y, color.rgb)
+                }
+            }
+        }
+
+        return outputImage
+    }
 }
 
 fun main() {
-    checkAndAssign()
-//    info(readln())
+    val input = InputImage("image")
+    val watermark = InputImage("watermark")
+    if (input.image.width < watermark.image.width && input.image.height < watermark.image.height) {
+        print("The watermark's dimensions are larger").also { exitProcess(0) }
+    }
+    if (watermark.image.transparency == 3) {
+        println("Do you want to use the watermark's Alpha channel?")
+        Blender.alpha = readln().lowercase() == "yes"
+    } else {
+        println("Do you want to set a transparency color?")
+        Blender.transparency = readln().lowercase() == "yes"
+        if (Blender.transparency) Blender.setTransparencyColor()
+    }
+    Blender.setTransparencyPercentage()
+
+    val output = OutputImage
+    output.createOutput(input.image, watermark.image)
 }
